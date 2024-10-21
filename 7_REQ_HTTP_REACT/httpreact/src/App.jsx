8b41d6e8c1a29e88bb620690f631eb8ef9aa3e7c
okarlheinz <okarlheinz@outlook.com>
2024-10-21 14:07:42 -0300

    Seção 7, aula 89

--- 7_REQ_HTTP_REACT/httpreact/src/App.jsx
+++ 7_REQ_HTTP_REACT/httpreact/src/App.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
 import './App.css'
 
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
 function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData(); 
+  }, []);
+
+  console.log(products);
 
   return (
     <>
