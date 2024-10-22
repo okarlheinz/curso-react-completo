9483a9fe05ce228d1810de2a27d187215a058897
Karl Heinz <110443535+okarlheinz@users.noreply.github.com>
2024-10-22 18:05:03 -0300

    Testando commit do PC da CDS

---
+++ src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css'
+
+import { useState, useEffect } from 'react'
+
+const url = "http://localhost:3000/products"
+
+function App() {
+  const [products, setProducts] = useState([]);
+
+  // 1 - resgatando dados
+  useEffect(() => {
+    async function fetchData() {
+      const res = await fetch(url);
+
+      const data = await res.json();
+
+      setProducts(data);
+    }
+
+    fetchData(); 
+  }, []);
+
+  console.log(products);
+
+  return (
+    <>
+      <h1>Lista de produtos</h1>
+      <h2>testando commit do pc da CDS</h2>
+    </>
+  )
+}
+
+export default App
